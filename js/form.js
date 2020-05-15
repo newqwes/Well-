0420b40b11012e4fa304382e634c292a47bc0790
Nazar Ashirov <newnewqwes@gmail.com>
2020-05-15 18:43:43 +0300

    work15.05.2020

---
+++ js/form.js
@@ -0,0 +1,34 @@
+// Проверяем с какого устройства передана форма
+let detect = new MobileDetect(window.navigator.userAgent);
+if (detect.mobile() !== null) {
+    $('[name*="device"]').val("Телефон или планшет: " + detect.mobile() + " Операционная система: " + detect.os() + " UserAgent: " + detect.userAgent());
+}
+else $('[name*="device"]').val("Компьютер");
+//  console.log("Mobile: " + detect.mobile());       // телефон или планшет 
+//  console.log("Phone: " + detect.phone());         // телефон 
+//  console.log("Tablet: " + detect.tablet());       // планшет 
+//  console.log("OS: " + detect.os());               // операционная система 
+//  console.log("userAgent: " + detect.userAgent()); // userAgent
+
+
+// Галочка после отправки формы
+jQuery(document).ready(function(){
+$('[name*="phone"]').mask("+375 (99) 99-99-999"); //маска номера телефона
+	jQuery("form").submit(function() { // Событие отправки с формы
+		var form_data = jQuery(this).serialize(); // Собираем данные из полей
+		jQuery.ajax({
+			type: "POST", // Метод отправки
+			url: "sendform.php", // Путь к PHP обработчику sendform.php
+			data: form_data,
+			success: swal({
+				title: "Спасибо за заявку в REFIT!",
+                type: "success",
+                showConfirmButton: false,
+                timer: 2000
+            })
+        });
+        $(this).find('input, textarea').prop('disabled', true); // блокируем повторный ввод данных
+        event.preventDefault();
+    });
+});
+
--- sendform.php
+++ sendform.php
@@ -1,19 +1,21 @@
 <?php
-//Сбор данных из полей формы. 
+//Сбор данных из полей формы.
+$device = $_POST['device'];
 $name = $_POST['name'];// Берём данные из input c атрибутом name="name"
-$phone = $_POST['phone']; // Берём данные из input c атрибутом name="phone"
-$email = $_POST['mail']; // Берём данные из input c атрибутом name="mail"
+$phone = str_replace("+", "", $_POST['phone']); // Берём данные из input c атрибутом name="phone"
+$text = $_POST['text']; // Берём данные из input c атрибутом name="mail"
 
 $token = "1274746361:AAFaEfCtiMMEPu_IThlaFWyP37QUKEcs96o"; // Тут пишем токен
 $chat_id = "-404256336"; // Тут пишем ID группы, куда будут отправляться сообщения
 $sitename = "5020.by"; //Указываем название сайта
-
+$newtel = 
 $arr = array(
   'Дата: ' => date('d-m-Y H:i:s'),
   'Заказ с сайта: ' => $sitename,
   'Имя: ' => $name,
-  'Телефон: ' => $phone,
-  'Почта' => $email
+  'Телефон: %2B' => $phone,
+  'Комментарий: ' => $text,
+  'Устройство: ' => $device
 );
 
 foreach($arr as $key => $value) {
