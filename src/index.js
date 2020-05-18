c64d1797c5d9308e0dc7dad360891df561e2d43d
Nazar Ashirov <newnewqwes@gmail.com>
2020-05-18 21:23:55 +0300

    smooth scroll to href id

--- src/index.js
+++ src/index.js
@@ -13,6 +13,13 @@ window.addEventListener('load', () => {
     }, 600);
 });
 
+//Scroll to id in main menu
+$("body").on('click', '[href*="#"]', function(e){
+    var fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
+
 // Проверяем с какого устройства передана форма
 let detect = new MobileDetect(window.navigator.userAgent);
 if (detect.mobile() !== null) {
