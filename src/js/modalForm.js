eb9fec55665074bf55d050035e4b4c8b8b2f72b2
Nazar Ashirov <newnewqwes@gmail.com>
2020-05-31 23:59:57 +0300

    modalJS done!

--- src/js/modalForm.js
+++ src/js/modalForm.js
@@ -1,26 +1,21 @@
-const BUTTON = document.getElementById('headerButton');
-const CLOSE_BUTTON = document.getElementById('modalBatton');
+const BUTTON = document.getElementById('headerButton'); 
+// заводим переменную для удобства. П.С. использовать лучше qverySelector
 
-BUTTON.addEventListener('click', () => {
+BUTTON.addEventListener('click', () => { 
+    // действие при нажатии на кнопку в шапке сайта, удаляем и добавляем другой класс для модалки, она внизу html
     document.getElementById('modalForm').classList.remove('modal__hidden');
     document.getElementById('modalForm').classList.add('modal__active');
     
 });
 
-// CLOSE_BUTTON.addEventListener('click', () => {
-//     document.getElementById('modalForm').classList.remove('modal__active');
-//     document.getElementById('modalForm').classList.add('modal__hidden');
-// });
-
-$(document).click(function (e) {
-    if (e.target === modal.modal__active) {
-        console.log('ewr');
+$(document).mouseup(function (e) { 
+    /* https://youtu.be/rIb0OoNWDEs?t=479 взял тут идею через jqvery всё равно подключил эту 
+    библиотеку, вообщем смысл в том что бы проверять на нажатие мыши и если не этот элемент 
+    главный в модалке и все его внутренности то закрываем модалку
+    */
+    let clickToOverlate = $('#modalBattonClose');
+    if (e.target != clickToOverlate[0]&&clickToOverlate.has(e.target).length === 0) {
+        document.getElementById('modalForm').classList.remove('modal__active');
+        document.getElementById('modalForm').classList.add('modal__hidden');
     }
 });
-
-document.onclick = function(e){
-    if ( event.target.className != 'modal__active' && event.target.className != 'header__button') {
-        document.getElementById('modalForm').classList.add('modal__hidden');
-        document.getElementById('modalForm').classList.remove('modal__active');
-    };
-};
