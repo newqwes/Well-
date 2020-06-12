8aad4cc173de756fbadd31a412b673f3816cd1b0
Nazar Ashirov <newnewqwes@gmail.com>
2020-06-13 00:50:57 +0300

    modefite from the submit form at chose model gajet

--- src/js/successful.js
+++ src/js/successful.js
@@ -1,23 +1,23 @@
-import swal from 'sweetalert'
+import swal from 'sweetalert';
 // Галочка после отправки формы
 jQuery(document).ready(function(){
-    $('[name*="phone"]').mask("+375 (99) 99-99-999") //маска номера телефона
-	jQuery("form").submit(function() { // Событие отправки с формы
-		const data = jQuery(this).serialize() // Собираем данные из полей
-		jQuery.ajax({
-			type: "POST", // Метод отправки
-			url: "sendform.php", // Путь к PHP обработчику sendform.php
-			data,
-			success: swal({
+    $('[name*="phone"]').mask("+375 (99) 99-99-999"); //маска номера телефона
+    jQuery(".contact-us__form, .footer__form, .modal__form").submit(function() { // Событие отправки с формы
+        const data = jQuery(this).serialize(); // Собираем данные из полей
+        jQuery.ajax({
+            type: "POST", // Метод отправки
+            url: "sendform.php", // Путь к PHP обработчику sendform.php
+            data,
+            success: swal({
                 title: "Спасибо!",
                 text: "REFIT благодарит за оставленную заявку!",
                 icon: "success",
                 button: false,
                 timer: 1500
-              })
-        })
-        $(this).find('input, textarea').prop('disabled', true) // блокируем повторный ввод данных
-        event.preventDefault()
-        document.getElementById('modalForm').classList.remove('modal__active')
-    })
-})
+                })
+        });
+        //$(this).find('input, textarea').prop('disabled', true); // блокируем повторный ввод данных
+        event.preventDefault();
+        document.getElementById('modalForm').classList.remove('modal__active');
+    });
+});
